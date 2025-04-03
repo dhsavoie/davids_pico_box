b878370b8f0e963340d36643f6fcf3a099e1199e
David Savoie <dhsavoie@gmail.com>
2025-04-02 23:44:09 -0400

    added ability to send hearts back and forth as reactions

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ from oled_graphics import *
 from machine import Pin, I2C
 from captive_portal import *
 from connect_to_wifi import *
-from my_secrets import pico_AP, pico_AP_pw, FIREBASE_URL, owner
+from my_secrets import pico_AP, pico_AP_pw, FIREBASE_MESSAGES_URL, FIREBASE_HEART_URL, owner, receiver
 
 debounce_delay = 500
 last_press_time = 0
+
+polling_delay = 3  # seconds
 
 unopened_messages = []
 opened_messages = []
@@ -23,13 +25,16 @@ envelope_open = False
 current_message_index = 0
 opened_message_index = 0
 
-button = Pin(0, Pin.IN, Pin.PULL_UP)
+heart_state = "full"
+
+scroll_button = Pin(0, Pin.IN, Pin.PULL_UP)
+heart_button = Pin(1, Pin.IN, Pin.PULL_UP)
 
 def check_messages():
     # unopened_messages = []
     global unopened_messages, new_message_waiting, opened_messages, opened_message_index
     try:
-        response = urequests.get(FIREBASE_URL)
+        response = urequests.get(FIREBASE_MESSAGES_URL)
         messages = response.json()
         response.close()
 
@@ -49,8 +54,30 @@ def check_messages():
     except Exception as e:
         print("Error fetching messages:", e)
 
+def check_heart():
+    global envelope_open, heart_state
+    try:
+        response = urequests.get(FIREBASE_HEART_URL)
+        heart = response.json()
+        response.close()
+
+        if heart:
+            for heart_id, heart_data in heart.items():
+                if heart_data.get("owner") == owner and not envelope_open:
+                    fb = framebuf.FrameBuffer(small_full_heart, 16, 8, framebuf.MONO_HLSB)
+                    display.blit(fb, 128-16, 64-8, framebuf.MONO_HLSB)
+                    display.show()
+                    heart_state = "full"
+                elif heart_data.get("owner") == receiver and not envelope_open:
+                    fb = framebuf.FrameBuffer(small_empty_heart, 16, 8, framebuf.MONO_HLSB)
+                    display.blit(fb, 128-16, 64-8, framebuf.MONO_HLSB)
+                    display.show()
+                    heart_state = "empty"
+    except Exception as e:
+        print("Error fetching heart:", e)
+
 def mark_message_as_read(msg_id):
-    update_url = f"{FIREBASE_URL[:-5]}/{msg_id}.json"
+    update_url = f"{FIREBASE_MESSAGES_URL[:-5]}/{msg_id}.json"
     update_data = ujson.dumps({"read": True})
     try:
         urequests.patch(update_url, data=update_data)
@@ -91,6 +118,28 @@ def handle_button_press(pin):
             display.fill_rect(0, 64-8, 8, 8, 0)
             display.text(f"{opened_message_index+1}/{len(opened_messages)}", 0, 64-8, 1)
             display.show()
+
+def handle_pass_heart(pin):
+    global heart_state, last_press_time, envelope_open
+
+    current_time = time.ticks_ms()
+    if time.ticks_diff(current_time, last_press_time) > debounce_delay:
+        last_press_time = current_time  # Update last press time
+
+        if heart_state == "full":
+            heart_state = "empty"
+            if not envelope_open:
+                fb = framebuf.FrameBuffer(small_empty_heart, 16, 8, framebuf.MONO_HLSB)
+                display.blit(fb, 128-16, 64-8, framebuf.MONO_HLSB)
+                display.show()
+
+            update_url = f"{FIREBASE_HEART_URL[:-5]}/heart.json"
+            update_data = ujson.dumps({"owner": receiver})
+            try:
+                urequests.patch(update_url, data=update_data)
+            except Exception as e:
+                print("Error updating heart:", e)
+
             
 
 
@@ -99,11 +148,12 @@ i2c = I2C(1, sda=Pin(18), scl=Pin(19), freq=400000)
 display = SH1106_I2C(128, 64, i2c, rotate=180)
 display.fill(0)  # Clear the display
 
-button.irq(trigger=Pin.IRQ_FALLING, handler=handle_button_press)
+scroll_button.irq(trigger=Pin.IRQ_FALLING, handler=handle_button_press)
+heart_button.irq(trigger=Pin.IRQ_FALLING, handler=handle_pass_heart)
 
 # first check if wifi credentials are saved
 if not connect_to_wifi():
-    display.display_wrapped_text("Starting AP. Connect to PicoSetup, password 12345678")
+    display.display_wrapped_text(f"Connect to wifi {pico_AP}, password {pico_AP_pw}, then enter wifi info at http:// 192.168.4.1")
     captive_portal()
 else:
     display.display_wrapped_text("Connected to WiFi!")
@@ -131,4 +181,6 @@ else:
                         display.show()
         previous_length = len(unopened_messages)
 
-        time.sleep(5)
+        check_heart()
+
+        time.sleep(polling_delay)
